c7bbf16140ed26b3309535a1dd63d07c506ea8f0
Idealosv <gonf7178@gmail.com>
2026-08-22 07:21:21 -0600

    feat: reemplaza panel DTE por modulo Facturacion

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import DteLauncher from './DteLauncher.jsx'
+import FacturacionLauncher from './FacturacionLauncher.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <DteLauncher />
+    <FacturacionLauncher />
   </StrictMode>,
 )
