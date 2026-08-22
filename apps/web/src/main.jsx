2b4fac408011650eb0bab6b790a76da5c3a6a176
Idealosv <gonf7178@gmail.com>
2026-08-22 07:32:00 -0600

    fix: cargar estilos visibles de mensajes de facturación

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import FacturacionLauncher from './FacturacionLauncher.jsx'
 import './styles.css'
+import './facturacion-feedback.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
