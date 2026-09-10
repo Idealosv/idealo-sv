7e6b1214a862cb1765a1fcadf20bbffc9f401dac
Idealosv <gonf7178@gmail.com>
2026-09-10 10:59:56 -0600

    security: restrict beverage catalog seed to authenticated users

---
+++ supabase/migrations/20260910111000_idealo_bar_beverage_catalog_permissions.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.bar_seed_complete_beverage_catalog(uuid) from public, anon;
+grant execute on function public.bar_seed_complete_beverage_catalog(uuid) to authenticated;
