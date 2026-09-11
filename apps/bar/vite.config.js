ee4abe88e9387a8f94e2b98ef5716c0de9054d1d
Idealosv <gonf7178@gmail.com>
2026-09-11 00:46:02 -0600

    style(bar): load cash command center styles

--- apps/bar/vite.config.js
+++ apps/bar/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import standaloneBarOrdersDashboard from './ordersDashboardPlugin.js'
 import standaloneBarCashDashboard from './cashDashboardPlugin.js'
+import standaloneBarCashStyles from './cashStylePlugin.js'
 
 function standaloneBarMenuNavigation(){
   const target='/apps/web/src/IdealoBarOperationsV2.jsx'
@@ -65,7 +66,7 @@ function standaloneBarMenuNavigation(){
 }
 
 export default defineConfig({
-  plugins: [standaloneBarMenuNavigation(),standaloneBarOrdersDashboard(),standaloneBarCashDashboard(),react()],
+  plugins: [standaloneBarMenuNavigation(),standaloneBarOrdersDashboard(),standaloneBarCashDashboard(),standaloneBarCashStyles(),react()],
   server: { host: '0.0.0.0' },
   preview: { host: '0.0.0.0' },
 })
