6bee8465751f84379428ff5256d947531375a715
Idealosv <gonf7178@gmail.com>
2026-09-10 21:12:32 -0600

    style(bar): load standalone menu navigation styles

--- apps/bar/vite.config.js
+++ apps/bar/vite.config.js
@@ -7,7 +7,7 @@ function standaloneBarMenuNavigation(){
     {
       label:'classification import',
       from:"import './idealo-bar-role-permissions.css'\n",
-      to:"import './idealo-bar-role-permissions.css'\nimport {TOP_LEVEL_GROUPS,getSubgroupsForGroup,filterStandaloneMenu} from '../../bar/src/menuNavigation.js'\n",
+      to:"import './idealo-bar-role-permissions.css'\nimport {TOP_LEVEL_GROUPS,getSubgroupsForGroup,filterStandaloneMenu} from '../../bar/src/menuNavigation.js'\nimport '../../bar/src/menu-navigation.css'\n",
     },
     {
       label:'two-level state',
